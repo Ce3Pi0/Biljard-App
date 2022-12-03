57fb0a179c5eec6f3eebe4c9f28d9223fc13ab06
Ce3Pi0 <hristijanbeast2004@gmail.com>
2022-12-03 22:30:23 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ setupIonicReact();
 const App: React.FC = () => (
   <GlobalProvider>
     <IonApp>
-      <IonReactRouter basename='Biljard-App'>
+      <IonReactRouter basename='/Biljard-App'>
         <IonTabs>
           <IonRouterOutlet>
             <Route exact path="/home">
